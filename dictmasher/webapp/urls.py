df9aa125bc97829b313466332ba7d73c77362142
Anthony <zilli@umich.edu>
2021-05-20 22:14:40 +0200

    Prod update 1

--- dictmasher/webapp/urls.py
+++ dictmasher/webapp/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.welcome, name='home'),
     path('settings/', views.settings, name='settings'),
     path('results/', views.results, name='results'),
-    path('about', views.about, name='about'),
-    path('success', views.success, name='success')
+    path('about/', views.about, name='about'),
+    path('success/', views.success, name='success')
 ]
 
 #test ints in seraches
