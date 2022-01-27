eaf776788760786409dbe77e1690fd32a64ea750
Anthony Zilli <zilli@umich.edu>
2022-01-27 23:37:44 +0100

    modified langdetect

--- detectlang.py
+++ detectlang.py
@@ -1,2 +1,3 @@
 # nltk? langid and langdetect don't seem very good for individual words
 # potential here: https://stackoverflow.com/questions/39142778/python-how-to-determine-the-language
+# due for deletion
